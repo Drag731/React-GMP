5af4168e8a3434ed2b703c48eeef151e559c331e
Oleksandr_Dragun <Oleksandr_Dragun@epam.com>
2018-05-25 13:27:11 +0300

    animation works correctly

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { CSSTransitionGroup } from 'react-transition-group'
+import { spring, AnimatedSwitch } from 'react-router-transition';
 
 import MainPage from './Features/MainPage/MainPage';
 import DescriptionPage from './Features/DescriptionPage/DescriptionPage';
@@ -12,25 +13,61 @@ import configureStore from './configure-store.js';
 
 import './style.scss'
 
+// we need to map the `scale` prop we define below
+// to the transform style property
+function mapStyles(styles) {
+    return {
+        opacity: styles.opacity,
+        transform: `scale(${styles.scale})`,
+    };
+}
+
+// wrap the `spring` helper to use a bouncy config
+function bounce(val) {
+    return spring(val, {
+        stiffness: 330,
+        damping: 22,
+    });
+}
+
+// child matches will...
+const bounceTransition = {
+    // start in a transparent, upscaled state
+    atEnter: {
+        opacity: 0,
+        scale: 1.2,
+    },
+    // leave in a transparent, downscaled state
+    atLeave: {
+        opacity: bounce(0),
+        scale: bounce(0.8),
+    },
+    // and rest at an opaque, normally-scaled state
+    atActive: {
+        opacity: bounce(1),
+        scale: bounce(1),
+    },
+};
+
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <Route render={({ location }) => (
-                <CSSTransitionGroup
-                    transitionName="page"
-                    transitionEnterTimeout={1000}
-                    transitionLeaveTimeout={1000}
+            <React.Fragment>
+                <AnimatedSwitch
+                    atEnter={bounceTransition.atEnter}
+                    atLeave={bounceTransition.atLeave}
+                    atActive={bounceTransition.atActive}
+                    mapStyles={mapStyles}
+                    className="route-wrapper"
                 >
-                    <Switch key={location.key} location={location}>
-                        <Route exact path="/filmzilla" component={MainPage} />
-                        <Route exact path="/filmzilla/description/:id" component={DescriptionPage} />
-                        <Route path="*" component={PageNotFound} />
-                    </Switch>
-                    <Footer/>
-                </CSSTransitionGroup>
-            )} />
+                    <Route exact path="/filmzilla" component={MainPage} />
+                    <Route exact path="/filmzilla/description/:id" component={DescriptionPage} />
+                    <Route path="*" component={PageNotFound} />
+                </AnimatedSwitch>
+                <Footer/>
+            </React.Fragment>
         </BrowserRouter>
     </Provider>, document.getElementById('root')
 );
